855c3d5aedd2707b12a5b3f4dd4d3d72693371e5
Heber <enredar@gmail.com>
2023-09-05 23:58:15 -0300

    Primeros pasos

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// selector
+    var menu = document.querySelector('.menu-header');
+    
+    // method
+    function toggleMenu (event) {
+      this.classList.toggle('is-active');
+      document.querySelector( ".menuppal" ).classList.toggle("is_active");
+      event.preventDefault();
+    }
+    
+    // event
+    menu.addEventListener('click', toggleMenu, false);
+    
+    //Solución con jQUery
+    /*$(document).ready(function(){
+        $('.menu-header').click(function() {
+            $('.menu-header').toggleClass('is-active');
+            $('.menuresponsive').toggleClass('is-active');
+            return false;
+        });
+    });*/
