4fe126b80fa2d7a725e82672762d2ee798c4bd7d
enredar <enredar@gmail.com>
2023-10-03 23:36:23 -0300

    Header y footer en el .js

--- README.md
+++
@@ -1,2 +0,0 @@
-# proyectocac40
-proyecto de pagina web para codo a codo 4.0
--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
-// selector
+    document.getElementById("header").innerHTML = `        
+    <div class="texto-header">
+    <h1><a href="index.html"><img src="img/logo.svg" alt="logo"></a> vemos hoy?</h1>
+    </div>
+    <div class="buscar-header">
+    <input type="text" class="buscar-header" name="searchInput" placeholder="Buscar...">
+    </div>
+    
+    <div class="menu-header">
+    <div class="_layer -top"></div>
+    <div class="_layer -mid"></div>
+    <div class="_layer -bottom"></div>
+    </div>
+    
+    <nav class="menuppal">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="usuario.html">Usuario</a></li>
+        <li><a href="peliculas.html">Películas</a></li>
+        <li><a href="series.html">Series</a></li>
+        <li><a href="recitales.html">Recitales</a></li>
+    </ul>
+    </nav>
+    </div>
+    `
+    
+    document.getElementById("footer").innerHTML = `
+    <div class="redes-sociales">
+    <a href="#"><i class="fab fa-facebook-f"></i></a>
+    <a href="#"><i class="fab fa-twitter"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+    <div class="contacto">
+    <a href="#"><i class="fas fa-envelope"></i> Correo Electrónico</a>
+    <a href="#"><i class="fas fa-phone"></i> Teléfono</a>
+    </div>
+    `
+
+    // selector
     var menu = document.querySelector('.menu-header');
     
     // method
@@ -19,5 +57,3 @@
             return false;
         });
     });*/
-
-
